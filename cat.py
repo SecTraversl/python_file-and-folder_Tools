e910996db7dbffc1764ca5cbb30f097d90d79702
Travis Logue <SecTraversl@ProtonMail.com>
2021-11-01 06:21:27 -0700

    Last write time 2021-11-01

---
+++ cat.py
@@ -0,0 +1,12 @@
+# %%
+#######################################
+def cat(thepath: str, as_bytes=False):
+    import pathlib
+
+    path_obj = pathlib.Path(thepath).resolve()
+    if as_bytes:
+        contents = path_obj.read_bytes()
+    else:
+        contents = path_obj.read_text()
+    return contents
+
